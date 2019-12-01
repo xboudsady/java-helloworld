b7fa46a01b9f714a50bfb68059dce2961a6fa671
xboudsady <boudsadyx@gmail.com>
2019-12-01 01:46:28 -0800

    Added Cars function

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,21 @@
+import java.awt.*;
+
 public class Main {
 
   public static void main(String[] args) {
     System.out.println("Hello World");
     System.out.println("Kathryn");
-    System.out.println("Woah, I'm progamming in Java!");
+    
+    Car myCar = new Car(25.5, "1BC32E", Color.BLUE, true);
+
+    Car sallyCar = new Car(13.9, "3D20BN", Color.BLACK, false);
+
+    System.out.println("My Car's License Plate: " + myCar.licensePlate);
+    System.out.println("Sally's License Plate: " + sallyCar.licensePlate);
+
+    System.out.println(myCar.paintColor.toString());
+    myCar.changePaintColor(Color.RED);
+    System.out.println(myCar.paintColor.toString());
+
   }
 }
