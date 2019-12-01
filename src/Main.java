d4d1f2c66753ef88ab668956ef98eaf2fa15a763
xboudsady <boudsadyx@gmail.com>
2019-12-01 01:25:07 -0800

    Initial commit, and created a Car class.

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+
+  public static void main(String[] args) {
+    System.out.println("Hello World");
+    System.out.println("Kathryn");
+    System.out.println("Woah, I'm progamming in Java!");
+  }
+}
