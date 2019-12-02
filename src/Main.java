8ba62344265a16fced30a60da85d13457c6344fb
xboudsady <boudsadyx@gmail.com>
2019-12-01 23:36:35 -0800

    Added user inputs

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,32 @@
 import java.awt.*;
+import java.util.Scanner;   // Allows us to get input from users
 
 public class Main {
 
   public static void main(String[] args) {
-    String userInput = "entertainment";                 // hardcode string
-    String uppercased = userInput.toUpperCase();        // call a string method to change to upercase
-    System.out.println(userInput);                      // return "entertainment"
-    System.out.println(uppercased);                     // prints "ENTERTAINMENT"
+    
+    // System.out.println("Enter a word:");      // User prompt for input
+    Scanner sc = new Scanner(System.in);      // Constructor method
 
-    char firstCharacter = userInput.charAt(0);          // method to get index 0 of string character
-    System.out.println(firstCharacter);                 // prints "e"
+    int userNumber = sc.nextInt();
+    System.out.println(userNumber);
+    
+    double userNumber2 = sc.nextDouble();
+    System.out.println(userNumber2);
 
-    System.out.println("Contains: " + userInput.contains("enter"));     // returns "true"
-    System.out.println("Contains: " + userInput.contains("Enter"));     // returns "false"
+    // String userInput = sc.next();             // Call the next() method to await user input
 
-    System.out.println("Contains: " + userInput.contains("Enter".toLowerCase()));   // returns "true"
+    // String uppercased = userInput.toUpperCase();        // call a string method to change to upercase
+    // System.out.println(userInput);                      // return "entertainment"
+    // System.out.println(uppercased);                     // prints "ENTERTAINMENT"
+
+    // char firstCharacter = userInput.charAt(0);          // method to get index 0 of string character
+    // System.out.println(firstCharacter);                 // prints "e"
+
+    // System.out.println("Contains: " + userInput.contains("enter"));     // returns "true"
+    // System.out.println("Contains: " + userInput.contains("Enter"));     // returns "false"
+
+    // System.out.println("Contains: " + userInput.contains("Enter".toLowerCase()));   // returns "true"
 
   }
 }
