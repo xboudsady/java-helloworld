2d1e28795a079bf934fc0b23681e90a07e322a81
xboudsady <boudsadyx@gmail.com>
2019-12-05 04:20:22 -0800

    Added secondary funtion to increase speed of car class object.

--- src/Car.java
+++ src/Car.java
@@ -2,22 +2,12 @@ import java.awt.*;
 
 public class Car {
 
-  // Data Types:
-  // int -> integer 1, 2, 3
-  // double -> decimal 34.5, 32.1
-  // String -> "a1a2" or "Hello World"
-  // Color -> from awt library
-  // boolean -> true or false
-
   double averageMilesPerGallon;
   String licensePlate;
   Color paintColor;
   boolean areTailingWorking;
 
-  public Car(double inputAverageMPG, 
-            String inputLicensePlate, 
-            Color inputPaintColor, 
-            boolean inputAreTailLightsWorking) {
+  public Car(double inputAverageMPG, String inputLicensePlate, Color inputPaintColor, boolean inputAreTailLightsWorking) {
     this.averageMilesPerGallon = inputAverageMPG;
     this.licensePlate = inputLicensePlate;
     this.paintColor = inputPaintColor;
@@ -26,6 +16,11 @@ public class Car {
 
   public void changePaintColor(Color newPaintColor) {
     this.paintColor = newPaintColor;
+  }
+
+  public double speedingUp(double currentSpeed) {
+    currentSpeed += 100;
+    return currentSpeed;
   }
 
 }
--- src/Main.java
+++ src/Main.java
@@ -1,33 +1,15 @@
 import java.awt.*;
-import java.lang.reflect.Array;
-import java.util.Arrays;
-import java.util.Scanner;
 
 public class Main {
 
   public static void main(String[] args) {
+    Car myCar = new Car(25.5, "1BC32E", Color.BLUE, true);
+
+    Car sallyCar = new Car(13.9, "3D20BN", Color.BLACK, false);
+
+    double myCarSpeed = 50;
+    myCarSpeed = myCar.speedingUp(myCarSpeed);
     
-    // 31,45,22, 98, 10
-    int[] numbers = new int[5];
-
-    numbers[0] = 31;
-    numbers[1] = 45;
-    numbers[2] = 22;
-    numbers[3] = 98;
-    numbers[4] = 10;
-
-    int[] numbers2 = {31, 45, 22, 98, 10};
-    Arrays.sort(numbers);
-    System.out.println(Arrays.toString(numbers));
-
-    String[] myFavoriteCandyBars = {"Twix", "Hershey's", "Crunch"};
-    System.out.println("Index 1: " + myFavoriteCandyBars[1]);
-
-    myFavoriteCandyBars[2] = "Butterfinger";
-    System.out.println("Index 2: " + myFavoriteCandyBars[2]);
-
-    System.out.println("Length: " + myFavoriteCandyBars.length);    
-
-    System.out.println(Array.get(myFavoriteCandyBars, 2));
+    System.out.println(myCarSpeed);
   }
 }
