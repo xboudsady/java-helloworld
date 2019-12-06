ffb29fc3c81238ed47449a0c1eda88fbf646f508
xboudsady <boudsadyx@gmail.com>
2019-12-05 21:10:36 -0800

    Using the java lang library to add extra functionality.

--- src/Main.java
+++ src/Main.java
@@ -4,19 +4,22 @@ import java.util.concurrent.Future;
 import java.util.Scanner;
 import java.util.Arrays;
 
+import java.util.Random;
+
 public class Main {
 
   public static void main(String[] args) {
     
-    // WHILE LOOP
-    System.out.println("While Loop");
-    int x = 3;
-    while (x > 0) {
-      System.out.println("Current x: " + x);
-      x = x - 1;
-    }
-    System.out.println("Final x: " + x);
-    System.out.println();
+    double power = Math.pow(5, 3);
+    System.out.println(power);
 
+    double squareRoot = Math.sqrt(64);
+    System.out.println(squareRoot);
+
+    Random rand = new Random();
+    int randomNumber = rand.nextInt();
+    int randomNumberWithBound = rand.nextInt(10);
+    System.out.println(randomNumber);
+    System.out.println(randomNumberWithBound);
   }
 }
